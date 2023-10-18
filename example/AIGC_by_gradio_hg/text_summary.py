84fdfe8f24a974b52ae84f6affb6faf9af80d89d
xiaoBai177 <2931215400@qq.com>
2023-10-18 21:49:06 +0800

    1. 文本总结模型：使用代理软件开启服务和代码代理 2. 聊天机器人（没看，用不太上）

--- example/AIGC_by_gradio_hg/text_summary.py
+++ example/AIGC_by_gradio_hg/text_summary.py
@@ -7,33 +7,25 @@
 @date: 2023/10/12 12:52
 @version: 1.0.0
 """
-from transformers import pipeline
 import os
-
-os.environ["http_proxy"] = "http://127.0.0.1:1080"
-os.environ["https_proxy"] = "http://127.0.0.1:1080"
+import gradio as gr
+from transformers import pipeline
+from dotenv import load_dotenv, find_dotenv
 
 
 def summarize(input_text):
-    def get_completion():
-        return pipeline("summarization")
-
-    output = get_completion()
-    return output[0]['summary_text']
+    return model(input_text)[0]['summary_text']
 
 
 if __name__ == "__main__":
-    text = ('''The tower is 324 metres (1,063 ft) tall, about the same height
-            as an 81-storey building, and the tallest structure in Paris. 
-            Its base is square, measuring 125 metres (410 ft) on each side. 
-            During its construction, the Eiffel Tower surpassed the Washington 
-            Monument to become the tallest man-made structure in the world,
-            a title it held for 41 years until the Chrysler Building
-            in New York City was finished in 1930. It was the first structure 
-            to reach a height of 300 metres. Due to the addition of a broadcasting 
-            aerial at the top of the tower in 1957, it is now taller than the 
-            Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the 
-            Eiffel Tower is the second tallest free-standing structure in France 
-            after the Millau Viaduct.''')
+    # 使用clash代理软件和代码配置系统代理环境，解决网络问题
+    _ = load_dotenv(find_dotenv("summary.env"))
 
-    print(summarize(text))
+    model = pipeline("summarization")
+    gr.close_all()
+    demo = gr.Interface(
+        fn = summarize,
+        inputs = "text",
+        outputs = "text",
+    )
+    demo.launch(share = True)
---
+++ example/matplotlib/point_cloud.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: Alan Tsang / Zeng Zhicun
+@institution: CSU, China, changsha
+@date: 2023/10/17 14:09
+@version: 1.0.0
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# 生成虚拟的点云数据
+num_points = 1000
+x = np.random.rand(num_points)  # 随机生成x坐标
+y = np.random.rand(num_points)  # 随机生成y坐标
+z = 0.1 * x + 0.2 * y + np.random.normal(0, 0.02, num_points)  # 生成z坐标，带一些噪声
+
+# 创建一个3D图形
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+# 绘制点云
+ax.scatter(x, y, z, c = 'b', marker = 'o')
+
+# 设置坐标轴标签
+ax.set_xlabel('X轴')
+ax.set_ylabel('Y轴')
+ax.set_zlabel('Z轴')
+
+# 显示图形
+plt.show()
