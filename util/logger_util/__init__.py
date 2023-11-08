4c82791a8805a1556ec9c75c6e6d3a0a488cfe4c
xiaoBai177 <2931215400@qq.com>
2023-11-08 11:28:26 +0800

    删除logging_util，已迁移至aspect, 删除tensorboard_util 没有必要有这个

--- util/logger_util/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-@author: Alan Tsang / Zeng Zhicun
-@institution: CSU, China, changsha
-@date: 2023/10/11 13:24
-@version: 1.0.0
-"""
--- util/tensorboard_util/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@author: Alan Tsang / Zeng Zhicun
-@institution: CSU, China, changsha
-@date: 2023/9/20 17:58
-@version: 1.0.0
-"""
-
